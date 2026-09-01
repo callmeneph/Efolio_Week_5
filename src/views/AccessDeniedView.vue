c578cb2aff72b8332a6ebc2f9f2561b21cc25d44
CallMeNephilim <mahdiafkar0@gmail.com>
2026-09-01 14:55:02 +1000

    access denied bro page

---
+++ src/views/AccessDeniedView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container text-center py-5">
+    <h2 class="text-danger">Access Denied! >:()</h2>
+    <p>Broo you are not authenticated to view this page, please go away and leave us in peace!</p>
+    <router-link to="/login" class="btn btn-primary">Go to Login</router-link>
+  </div>
+</template>
